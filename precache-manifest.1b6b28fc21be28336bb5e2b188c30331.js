a52975b3d28db89f80df6f5a24841410982b922c
Lan-zifang <13190890+lan-zifang@user.noreply.gitee.com>
2024-06-24 22:14:42 +0800

    deploy

---
+++ precache-manifest.1b6b28fc21be28336bb5e2b188c30331.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30fba2d69250d045727e",
+    "url": "/wangwang-website/css/app.5ae562cc.css"
+  },
+  {
+    "revision": "a58914c77b5261c77830a9f7af0d7ab1",
+    "url": "/wangwang-website/index.html"
+  },
+  {
+    "revision": "30fba2d69250d045727e",
+    "url": "/wangwang-website/js/app.7f212e61.js"
+  },
+  {
+    "revision": "799025c9881e5a2a8a2f",
+    "url": "/wangwang-website/js/chunk-vendors.c0bc88ef.js"
+  },
+  {
+    "revision": "3536e49524be93ab1ea0cfe4f6308c30",
+    "url": "/wangwang-website/manifest.json"
+  },
+  {
+    "revision": "7b5c76071bba7f4dcecfd82d378d1b20",
+    "url": "/wangwang-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wangwang-website/robots.txt"
+  }
+]);
